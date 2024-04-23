f264394f49513444cd7ff235263820da85aeef5d
unkempt <rich@aypstudio.com>
2024-04-23 08:30:09 +0100

    Update index.ts
    user 3

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe 2';
+  const name = 'John Doe 3';
   greetUser(name);
+  document.body.style.backgroundColor = 'red';
 });
