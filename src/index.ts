cc38bc6c034cfb8828f7d9f69a4fba02f8ac558f
unkempt <rich@aypstudio.com>
2024-04-23 11:01:14 +0100

    ff

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe 7';
+  const name = 'John Doe 8';
   greetUser(name);
-  document.body.style.backgroundColor = 'red';
+  document.body.style.backgroundColor = 'yellow';
 });
